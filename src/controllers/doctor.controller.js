177663929c19466a68567ce4deadfb1f138cf3cb
Nitesh Prajapati <niteshkmrprajapati298@gmail.com>
2025-08-19 18:07:31 +0530

    pdateDoctorAvailability-controller+route

--- src/controllers/doctor.controller.js
+++ src/controllers/doctor.controller.js
@@ -3,53 +3,54 @@ const DoctorModel = require("../models/doctor.model");
 const { createDoctor } = require("../services/doctor.service");
 
 module.exports.registerDoctor = async (req, res, next) => {
-    try {
-        const { firstName, lastName, email, password, specialization } = req.body;
-        const isDoctorAlreadyExists = await DoctorModel.findOne({ email: email });
-        if (isDoctorAlreadyExists) return res.status(401).json("Email Id is already Registered with an Account");
+  try {
+    const { firstName, lastName, email, password, specialization } = req.body;
+    const isDoctorAlreadyExists = await DoctorModel.findOne({ email: email });
+    if (isDoctorAlreadyExists) return res.status(401).json("Email Id is already Registered with an Account");
 
-        const doctor = await createDoctor(firstName, lastName, email, password, specialization);
-        const token = await doctor.getJwt();
-        res.cookie('token', token);
-        res.status(201).json({ token: token, message: "Doctor Registered Successfully" })
-    } catch (error) {
-        res.status(401).json(error.message)
-    }
+    const doctor = await createDoctor(firstName, lastName, email, password, specialization);
+    const token = await doctor.getJwt();
+    res.cookie('token', token);
+    res.status(201).json({ token: token, message: "Doctor Registered Successfully" })
+  } catch (error) {
+    res.status(401).json(error.message)
+  }
 }
 module.exports.loginDoctor = async (req, res, next) => {
-    try {
-        const { email, password } = req.body;
-        if (!email || !password) return res.status(401).json("All Fields are Required");
-        const doctor =await DoctorModel.findOne({ email }).select('+password');
-        if (!doctor) return res.status(401).json("Invalid Credentials");
-        const isHashPassword =await doctor.validatePassword(password);
-        if (!isHashPassword) return res.status(401).json("Invalid Credentials");
-       
-        const token =await doctor.getJwt();
-        res.cookie("token",token);
-        res.status(201).json({ token: token, message: "User Logged In successfully" })
-    } catch (error) {
-        res.status(401).json( error.message)}
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) return res.status(401).json("All Fields are Required");
+    const doctor = await DoctorModel.findOne({ email }).select('+password');
+    if (!doctor) return res.status(401).json("Invalid Credentials");
+    const isHashPassword = await doctor.validatePassword(password);
+    if (!isHashPassword) return res.status(401).json("Invalid Credentials");
+
+    const token = await doctor.getJwt();
+    res.cookie("token", token);
+    res.status(201).json({ token: token, message: "User Logged In successfully" })
+  } catch (error) {
+    res.status(401).json(error.message)
+  }
 }
 
-module.exports.getDoctorProfile = async (req,res,next)=>{
-    try {
-        const doctor = req.doctor;
-        if(!doctor) return res.status(401).json("Unable to Fetch Profile");
-        res.status(200).json({YourProfile:doctor});
-    } catch (error) {
-        return res.status(404).json(error.message,"Internal Server Error")
-    }
+module.exports.getDoctorProfile = async (req, res, next) => {
+  try {
+    const doctor = req.doctor;
+    if (!doctor) return res.status(401).json("Unable to Fetch Profile");
+    res.status(200).json({ YourProfile: doctor });
+  } catch (error) {
+    return res.status(404).json(error.message, "Internal Server Error")
+  }
 }
-module.exports.logoutDoctor = async (req,res,next) => {
-    try {
-        const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
-        if(!token) return res.status(401).json("Token Not Found Please LogIn");
-        res.clearCookie("token",token);
-        res.status(201).json({message:"Logged Out Successfully"})
-    } catch (error) {
-        res.status(401).json("Internal Server Error");
-    }
+module.exports.logoutDoctor = async (req, res, next) => {
+  try {
+    const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
+    if (!token) return res.status(401).json("Token Not Found Please LogIn");
+    res.clearCookie("token", token);
+    res.status(201).json({ message: "Logged Out Successfully" })
+  } catch (error) {
+    res.status(401).json("Internal Server Error");
+  }
 }
 module.exports.getDoctors = async (req, res) => {
   try {
@@ -84,4 +85,28 @@ module.exports.getDoctorsBySpeciality = async (req, res) => {
   }
 };
 
-  
+
+
+module.exports.updateDoctorAvailability = async (req, res) => {
+  try {
+    const { status } = req.body;
+    if (status !== "Active" && status !== "inactive") return res.status(400).json("Please Enter a valid status");
+
+    const doctorId = req.doctor._id;
+    if (!doctorId) return res.status(400).json("Invalid or Expiry Token");
+    const doctor = await DoctorModel.findByIdAndUpdate(
+      doctorId,
+      { status: status },
+      {new:true}
+    );
+    return res.status(200).json({
+      message:"Status Update Successfully",
+      doctor
+    })
+
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+}
+
+
--- src/routes/doctor.routes.js
+++ src/routes/doctor.routes.js
@@ -1,10 +1,26 @@
 
 const express = require('express');
-const { registerDoctor, loginDoctor, getDoctorProfile, logoutDoctor } = require('../controllers/doctor.controller');
+const { registerDoctor, loginDoctor, getDoctorProfile, logoutDoctor, updateDoctorProfile, updateDoctorAvailability } = require('../controllers/doctor.controller');
 const { authDoctor } = require('../middlewares/auth.middleware');
 const router = express.Router();
 router.post('/signup',registerDoctor);
 router.post('/login',loginDoctor);
 router.get('/profileView',authDoctor,getDoctorProfile);
 router.post('/logout',authDoctor,logoutDoctor);
+router.patch("/availability", authDoctor, updateDoctorAvailability);
+
+
+// 🔑 Doctor Routes
+
+// ✅ Already have: register, login, profile, logout
+// 👉 Add these:
+
+// PATCH /doctor/availability → to update doctor availability (when schedule changes).
+
+// GET /doctor/appointments → doctor can view all upcoming/past appointments.
+
+// PATCH /doctor/appointment/:id/complete → doctor marks an appointment as completed (if not handled by cron).
+
+// PATCH /doctor/appointment/:id/cancel → doctor cancels appointment if unavailable.
+
 module.exports=router;
